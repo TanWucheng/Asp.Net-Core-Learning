148135df48416a13b8e8df7d3f9cdfe1492234bc
TanWucheng <tanwucheng@outlook.com>
2021-02-06 00:53:29 +0800

    Commit 2021-02-06 00:53:00

---
+++ IdentityServer4Book/ConsoleClient/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Net.Http;
+using IdentityModel.Client;
+
+namespace ConsoleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            GetToken();
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey(true);
+        }
+
+        private static async void GetToken()
+        {
+            var httpClient = new HttpClient();
+
+            var disco = await httpClient.GetDiscoveryDocumentAsync("https://localhost:5001");
+            if (disco.IsError)
+            {
+                Console.WriteLine("Discover Document Error:");
+                Console.WriteLine(disco.Error);
+                return;
+            }
+
+            var tokenResponse = await httpClient.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+            {
+                Address = disco.TokenEndpoint,
+                ClientId = "clientbook",
+                ClientSecret = "secretbook",
+                Scope = "apibook"
+            });
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine("Get Token Error:");
+                Console.WriteLine(tokenResponse.Error);
+                return;
+            }
+
+            Console.WriteLine("Token Response:");
+            Console.WriteLine(tokenResponse.Json);
+
+            var apiClient = new HttpClient();
+            apiClient.SetBearerToken(tokenResponse.AccessToken);
+
+            var apiResponse = await apiClient.GetAsync("https://localhost:5003/identity");
+            if (!apiResponse.IsSuccessStatusCode)
+            {
+                Console.WriteLine("Api Response Error Code:");
+                Console.WriteLine(apiResponse.StatusCode);
+            }
+            else
+            {
+                var content = await apiResponse.Content.ReadAsStringAsync();
+                Console.WriteLine("Api Response Content String:");
+                Console.WriteLine(content);
+            }
+        }
+    }
+}
---
+++ IdentityServer4Book/IdentityServer/Config.cs
@@ -0,0 +1,44 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace IdentityServer
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new IdentityResource[]
+            {
+                new IdentityResources.OpenId()
+            };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new ApiScope[]
+            {
+                new ApiScope("apibook", "ASP.NET Core Book API")
+            };
+
+        public static IEnumerable<Client> Clients =>
+            new Client[]
+            {
+                new Client
+                {
+                    ClientId = "clientbook",
+
+                    // 没有交互式用户，使用ClientId/ClientSecrets进行身份验证
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                    // 认证ClientId/ClientSecrets
+                    ClientSecrets = {new Secret("secretbook".Sha256())},
+
+                    // 客户端有权访问的范围
+                    AllowedScopes = {"apibook"}
+                }
+            };
+
+        public static IEnumerable<ApiResource> ApiResources => new[] {new ApiResource("apibook", "ASP.NET Core Book API")};
+    }
+}
